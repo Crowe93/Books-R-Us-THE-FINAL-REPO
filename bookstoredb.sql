64a7c2bd6c099d8648b4ba1788189edf418c6fbf
Michael Tostenson <mat@uga.edu>
2017-07-26 16:53:11 +0000

    Add files via upload

--- bookstoredb.sql
+++ bookstoredb.sql
@@ -34,10 +34,10 @@ CREATE TABLE `book` (
   `price` double DEFAULT NULL COMMENT 'Holds book price',
   `stock` int(11) DEFAULT NULL COMMENT 'Holds current amount in inventory',
   `sold` int(11) DEFAULT NULL COMMENT 'Holds number sold',
-  `imgURL` varchar(100) DEFAULT NULL,
+  `imgURL` varchar(256) DEFAULT NULL,
   `minimum` int(11) DEFAULT '2',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -46,7 +46,7 @@ CREATE TABLE `book` (
 
 LOCK TABLES `book` WRITE;
 /*!40000 ALTER TABLE `book` DISABLE KEYS */;
-INSERT INTO `book` VALUES (1,'test','test','test','test',1999,10,5,0,'https://cdn.vox-cdn.com/uploads/chorus_asset/file/8552631/leather_book_preview.png',3),(2,'The Kite Runner','Khaled Hosseini','Riverhead Books','Drama',2003,15,5,0,'https://upload.wikimedia.org/wikipedia/en/6/62/Kite_runner.jpg',3),(3,'Hitchhiker\'s Guide To The Galaxy','Douglas Adams','Pan Books','Science Fiction',1979,15,5,0,'https://images-na.ssl-images-amazon.com/images/I/A1HGWCA36hL.jpg',3),(4,'Lord of the Rings: The Fellowship of the Ring','J. R. R. Tolkien','Allen & Unwin','Fantasy',1954,15,5,0,'https://images-na.ssl-images-amazon.com/images/I/41kUPvqlguL.jpg',3),(6,'Lord of the Rings: The Two Towers','J. R. R. Tolkien','Allen & Unwin','Fantasy',1954,15,5,0,'https://images-na.ssl-images-amazon.com/images/I/41cnYEiew3L._SX304_BO1,204,203,200_.jpg',3),(7,'Lord of the Rings: The Return of the King','J. R. R. Tolkien','Allen & Unwin','Fantasy',1955,15,5,0,'https://i.stack.imgur.com/071TF.jpg',3),(8,'A Tale of Two Cities','Charles Dickens','Signet Classics','History',1859,12,5,0,'http://images.gr-assets.com/books/1410762334l/135625.jpg',2),(9,'The Adventures of Sherlock Holmes','Arthur Conan Doyle','George Newnes','Mystery',1892,12,5,0,'http://t2.gstatic.com/images?q=tbn:ANd9GcStWjErePCa5zwizPuciyRBDXWQY8fHKcyvrzegbxAdcA8ZN5ms',2),(10,'The Hunt for Red October','Tom Clancy','Naval Institute Press','Action',1984,12,5,0,'http://t2.gstatic.com/images?q=tbn:ANd9GcR0r7EouCdLxXfyIUU1QCGGKrGrh7v35ACAqXLz0hoTjaEtQ5gI',2),(11,'The Handmaid\'s Tale','Margaret Atwood','McClelland and Stewart','Science Fiction',1985,15,5,0,'https://images-na.ssl-images-amazon.com/images/I/41aPpkv7ZjL._SY346_.jpg',2),(12,'A Dirty Job','Christopher Moore','Harper Collins','Comedy',2006,12,5,0,'http://ecx.images-amazon.com/images/I/51Afkc1pDEL._SX329_BO1,204,203,200_.jpg',2);
+INSERT INTO `book` VALUES (2,'The Kite Runner','Khaled Hosseini','Riverhead Books','Drama',2003,15,5,0,'https://upload.wikimedia.org/wikipedia/en/6/62/Kite_runner.jpg',3),(3,'Hitchhiker\'s Guide To The Galaxy','Douglas Adams','Pan Books','Science Fiction',1979,15,5,0,'https://images-na.ssl-images-amazon.com/images/I/A1HGWCA36hL.jpg',3),(4,'Lord of the Rings: The Fellowship of the Ring','J. R. R. Tolkien','Allen & Unwin','Fantasy',1954,15,5,0,'https://images-na.ssl-images-amazon.com/images/I/41kUPvqlguL.jpg',3),(6,'Lord of the Rings: The Two Towers','J. R. R. Tolkien','Allen & Unwin','Fantasy',1954,15,5,0,'https://images-na.ssl-images-amazon.com/images/I/41cnYEiew3L._SX304_BO1,204,203,200_.jpg',3),(7,'Lord of the Rings: The Return of the King','J. R. R. Tolkien','Allen & Unwin','Fantasy',1955,15,5,0,'https://i.stack.imgur.com/071TF.jpg',3),(8,'A Tale of Two Cities','Charles Dickens','Signet Classics','History',1859,10,5,0,'http://images.gr-assets.com/books/1410762334l/135625.jpg',2),(9,'The Adventures of Sherlock Holmes','Arthur Conan Doyle','George Newnes','Mystery',1892,12,5,0,'http://t2.gstatic.com/images?q=tbn:ANd9GcStWjErePCa5zwizPuciyRBDXWQY8fHKcyvrzegbxAdcA8ZN5ms',2),(10,'The Hunt for Red October','Tom Clancy','Naval Institute Press','Action',1984,12,5,0,'http://t2.gstatic.com/images?q=tbn:ANd9GcR0r7EouCdLxXfyIUU1QCGGKrGrh7v35ACAqXLz0hoTjaEtQ5gI',2),(11,'The Handmaid\'s Tale','Margaret Atwood','McClelland and Stewart','Science Fiction',1985,15,5,0,'https://images-na.ssl-images-amazon.com/images/I/41aPpkv7ZjL._SY346_.jpg',2),(12,'A Dirty Job','Christopher Moore','Harper Collins','Comedy',2006,12,5,0,'http://ecx.images-amazon.com/images/I/51Afkc1pDEL._SX329_BO1,204,203,200_.jpg',2),(13,'The Cat In The Hat','Dr. Seuss','Houghton Mifflin Harcourt','Comedy',1954,8,5,0,'https://upload.wikimedia.org/wikipedia/en/thumb/b/b5/Seuss-cat-hat.gif/220px-Seuss-cat-hat.gif',3),(14,'Green Eggs and Ham','Dr. Seuss','Houghton Mifflin Harcourt','Comedy',1960,8,5,0,'https://upload.wikimedia.org/wikipedia/en/thumb/1/11/Green_Eggs_and_Ham.jpg/220px-Green_Eggs_and_Ham.jpg',2),(15,'Oh, the Places You\'ll Go!','Dr. Seuss','Random House','Comedy',1990,8,5,0,'https://images-na.ssl-images-amazon.com/images/I/518eq5NjZkL._SX359_BO1,204,203,200_.jpg',3),(16,'The Lorax','Dr. Seuss','Random House','Comedy',1971,8,5,0,'https://upload.wikimedia.org/wikipedia/en/1/13/The_Lorax.jpg',3),(17,'The Butter Battle Book','Dr. Seuss','Random House','Comedy',1984,5,5,0,'https://images-na.ssl-images-amazon.com/images/I/51MXLw7mSWL._SY344_BO1,204,203,200_.jpg',2),(18,'A Game of Thrones','George R.R. Martin','Bantam Spectra','Fantasy',1996,18,8,0,'https://images-na.ssl-images-amazon.com/images/I/919-FLL37TL.jpg',5),(19,'A Clash of Kings','George R.R. Martin','Bantam Spectra','Fantasy',1998,18,8,0,'https://images-na.ssl-images-amazon.com/images/I/81m8bFyfGaL.jpg',5),(20,'A Strom of Sword','George R.R. Martin','Bantam Spectra','Fantasy',2000,18,8,0,'https://images-na.ssl-images-amazon.com/images/I/81xHKn-bXSL.jpg',5),(21,'A Feast for Crows','George R.R. Martin','Bantam Spectra','Fantasy',2005,18,8,0,'https://images-na.ssl-images-amazon.com/images/I/71nJQs9rawL.jpg',5),(22,'A Dance with Dragons','George R.R. Martin','Bantam Spectra','Fantasy',2011,20,8,0,'https://images-na.ssl-images-amazon.com/images/I/51oe-hbvKTL._SX301_BO1,204,203,200_.jpg',5);
 /*!40000 ALTER TABLE `book` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -206,9 +206,9 @@ DROP TABLE IF EXISTS `payment`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `payment` (
   `user_id` int(11) NOT NULL,
-  `cardNum` varchar(16) NOT NULL,
-  `expDate` varchar(7) DEFAULT NULL,
-  `csc` varchar(3) DEFAULT NULL,
+  `cardNum` varchar(45) NOT NULL,
+  `expDate` varchar(10) DEFAULT NULL,
+  `csc` varchar(10) DEFAULT NULL,
   `cardType` varchar(100) DEFAULT NULL,
   `billingAddr` varchar(100) DEFAULT NULL,
   KEY `user_id_idx` (`user_id`),
@@ -322,7 +322,7 @@ CREATE TABLE `user` (
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -331,7 +331,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Chris','Rowe','ccr','pass','ccr@uga.edu',0),(4,'Michael','Tostenson','mat','pass','mat@uga.edu',0),(8,'Justin','Chiu','jmc','pass','jmc@uga.edu',0),(9,'Eric','Holbrook','ebh','pass','ebh@uga.edu',0),(10,'Chanice','Campbell','cmc','pass','cmc@uga.edu',0),(11,'BookStore','Owner','admin','admin','admin@bookstore.com',1);
+INSERT INTO `user` VALUES (1,'Chris','Rowe','ccr','pass','ccr@uga.edu',0),(4,'Michael','Tostenson','mat','pass','mat@uga.edu',0),(8,'Justin','Chiu','jmc','pass','jmc@uga.edu',0),(9,'Eric','Holbrook','ebh','pass','ebh@uga.edu',0),(10,'Chanice','Campbell','cmc','pass','cmc@uga.edu',0),(11,'BookStore','Owner','admin','admin','admin@bookstore.com',1),(12,'Joe','Chill','deathToWayne','bang','pearls@alleyway.com',0),(13,'Jason','Todd','redhood','bats','badGuysBeware@batcave.com',0),(14,'Oswald','Cobblepot','umbrellaMan','penguin','blackandwhite@badguy.com',0),(15,'Tony','Stark','IronGuy22','pepper','rocketman@stark.inc',0),(16,'Luke','Cage','iron_sides','jess','unstoppable@defender.com',0),(17,'Barbra','Gordon','oracle','batgirl','daddysGirl@GCPD.com',0);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -352,4 +352,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-26 10:21:13
+-- Dump completed on 2017-07-26 12:52:23
